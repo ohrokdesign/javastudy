5aaeab861e2768fb18cbede9babc44e4d3691ce9
Sang-hun <ohrokdesign@gmail.com>
2021-04-13 16:40:44 +0900

    Scanner Study

---
+++ basic/src/Scanner01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Scanner01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name;
+        int Math;
+
+        System.out.print("이름? : ");
+        name = sc.nextLine();
+
+        System.out.print("점수? : ");
+        Math = sc.nextInt();
+
+        System.out.println(name+" : "+Math);
+    }
+}
