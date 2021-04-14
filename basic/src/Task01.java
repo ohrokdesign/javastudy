97875c3a116167b954575f8f4df2515c06f99d81
Sang-hun <ohrokdesign@gmail.com>
2021-04-14 10:05:59 +0900

    2nd trial

--- basic/src/Task01.java
+++ basic/src/Task01.java
@@ -4,20 +4,21 @@
 
 public class Task01 {
     public static void main(String[] args) {
-        for (int n = 2; n <= 100000; n++) {
+        int count = 0;
+        for (int n = 2; n <= 100; n++) {
             boolean isPrime = true;
-            int j=0;
             for (int i = 2; i * i <= n && isPrime; i++) {
                 if (n%i ==0){
                     isPrime = false;
 
                 }
                 if (isPrime){
-                    j++;
+                    count = count++;
                 }
+                
             }
-            System.out.println(j);
         }
+        System.out.println(count);
     }
 
 }
