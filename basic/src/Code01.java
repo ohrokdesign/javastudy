678db8e8d5e9e3b03fd608a9917547da19ff1764
Sang-hun <ohrokdesign@gmail.com>
2021-04-14 10:23:30 +0900

    code01 added

---
+++ basic/src/Code01.java
@@ -0,0 +1,11 @@
+public class Code01 {
+    public static int num;
+    public static void main(String[] args) {
+        int anotherNum = 2;
+        num = 10;
+
+        System.out.println(anotherNum+num);
+        System.out.println("anotherNum"+"num");
+        System.out.println("anotherNum+num");
+    }
+}
