557f4bb08486bdf7fc637db264548f9ee037143d
Sang-hun <ohrokdesign@gmail.com>
2021-04-17 18:16:46 +0900

    array Code05

---
+++ basic/src/Code05.java
@@ -0,0 +1,17 @@
+//배열을 해보자 !!
+
+public class Code05 {
+    public static void main(String[] args) {
+        int []grades;
+        
+        grades = new int[5];
+
+        grades[0] = 12;
+        grades[1] = 35;
+        grades[2] = 40;
+        grades[3] = 60;
+        grades[4] = 1;
+
+        System.out.println(grades[3]);
+    }
+}
