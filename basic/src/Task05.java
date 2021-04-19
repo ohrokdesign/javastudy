1278edfe1eb7cb8ea3c74a50e354979b897ed928
hi-oslab <62414811+ohrokdesign@users.noreply.github.com>
2021-04-19 23:19:14 +0900

    Task05...... 하는 중
    으악 집중안돼

---
+++ basic/src/Task05.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task05 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int []data = new int[n];
+
+        for (int i = 0; i < arrayS; i++) {
+            data[i] = kb.nextInt();
+        }
+        kb.close();
+
+        int sum=0
+    }
+}
